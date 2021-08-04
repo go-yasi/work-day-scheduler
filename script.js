40c608a62292707685947a3309b4d38edeee01d6
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-08-03 18:48:43 -0700

    deleted some notes and added back save button variable

--- script.js
+++ script.js
@@ -3,27 +3,13 @@ var currentTime = moment().format("H");
 $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
 
 var plannerDiv = $(".container");
+var saveBtn = $(".saveBtn");
 // var plannerRow = $("<div>");
 // var hour = $("<div>");
 // var activity = $("<div>");
-// var saveBtn = $("<button>");
-
-// plannerRow.addClass("row");
-
-// plannerRow.text("test");
-
-// plannerDiv.append(plannerRow);
-
-// identify the current time so I can add the .present class to the current hour row
 
 
 
-// if currentTime =  id add class .present
-// for (var i = 9; i < 18; i++) {
-//     if (currentTime === i) {
-//         $(".hour").addClass(".present");
-//     }
-// };
 
 if (currentTime < 9) {
     for(var i = 0; i < plannerDiv.children().length; i++){
@@ -44,7 +30,3 @@ if (currentTime < 9) {
         }
     }
 }
-
-// using jquery this to target what we need from each div and comparing it to moment
-// if you need to add future class remember to remove present class
-// what to do for local storage
